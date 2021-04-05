ba42a18db235505eba9e5bc0a0e2ac18cb4b6a4f
rowantseng <wanju.ts@gmail.com>
2021-04-05 16:40:23 +0800

    add node intro

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+
+var superheroes = require("superheroes");
+var mySuperHero = superheroes.random();
+console.log(mySuperHero);
+
+var supervillains = require("supervillains");
+var mySuperVillain = supervillains.random();
+console.log(mySuperVillain);
