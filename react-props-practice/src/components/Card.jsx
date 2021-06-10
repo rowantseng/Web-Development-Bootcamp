e5ec09ff4d315bb3d86a2d7d8c0b28e8733fa46b
rowantseng <wanju.ts@gmail.com>
2021-06-10 17:14:14 +0800

    add react props practice

---
+++ react-props-practice/src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Details from "./Details";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <Avatar imgURL={props.imgURL} />
+      </div>
+      <div className="bottom">
+        <Details info={props.phone}/>
+        <Details info={props.email}/>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ react-props-practice/src/components/Details.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Details(props) {
+  return (
+    <div className="info">
+      <p>{props.info}</p>
+    </div>
+  );
+}
+
+export default Details;
