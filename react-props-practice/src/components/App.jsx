e4831efec9b656fbb887ae263767616896c0376b
rowantseng <wanju.ts@gmail.com>
2021-06-10 17:24:38 +0800

    add map function for looping

--- react-props-practice/src/components/App.jsx
+++ react-props-practice/src/components/App.jsx
@@ -1,32 +1,24 @@
 import React from "react";
-import Card from "./Card"
+import Card from "./Card";
 import contacts from "../contacts";
+
+function CreateCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      imgURL={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
-      <Card
-        name={contacts[0].name}
-        imgURL={contacts[0].imgURL}
-        phone={contacts[0].phone}
-        email={contacts[0].email}
-      />
-
-      <Card
-        name={contacts[1].name}
-        imgURL={contacts[1].imgURL}
-        phone={contacts[1].phone}
-        email={contacts[1].email}
-      />
-
-      <Card
-        name={contacts[2].name}
-        imgURL={contacts[2].imgURL}
-        phone={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(CreateCard)}
     </div>
   );
 }
