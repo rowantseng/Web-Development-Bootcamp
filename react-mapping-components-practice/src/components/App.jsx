283bdc1ba8174fa1f579ca3f0773a8e66e7c884b
rowantseng <wanju.ts@gmail.com>
2021-06-10 17:39:23 +0800

    compose emojipedia with components

--- react-mapping-components-practice/src/components/App.jsx
+++ react-mapping-components-practice/src/components/App.jsx
@@ -1,4 +1,17 @@
 import React from "react";
+import emojipedia from "../emojipedia";
+import Entry from "./Entry";
+
+function CreateEntry(entry) {
+  return (
+    <Entry
+      key={entry.id}
+      emoji={entry.emoji}
+      name={entry.name}
+      meaning={entry.meaning}
+    />
+  );
+}
 
 function App() {
   return (
@@ -8,44 +21,7 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
+        {emojipedia.map(CreateEntry)}
       </dl>
     </div>
   );
