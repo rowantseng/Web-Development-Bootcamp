7756815de3fd028be478641b314c7a8261f9b635
rowantseng <wanju.ts@gmail.com>
2021-06-11 17:32:30 +0800

    add usestate-hook-practice challenge 1

--- react-usestate-hook-practice/src/components/App.jsx
+++ react-usestate-hook-practice/src/components/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const nowTime = new Date().toLocaleTimeString("en-GB");
+  const [time, setTime] = useState(nowTime);
+
+  function Click() {
+    const newTime = new Date().toLocaleTimeString("en-GB");
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={Click}>Get Time</button>
     </div>
   );
 }
--- react-usestate-hook-practice/src/index.js
+++ react-usestate-hook-practice/src/index.js
@@ -6,8 +6,6 @@ ReactDOM.render(<App />, document.getElementById("root"));
 
 //Challenge:
 //1. Given that you can get the current time using:
-let time = new Date().toLocaleTimeString();
-console.log(time);
 //Show the latest time in the <h1> when the Get Time button
 //is pressed.
 
