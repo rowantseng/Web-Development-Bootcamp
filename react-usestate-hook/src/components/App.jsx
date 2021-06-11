2b2bd2f4e7324806d0cc9e522eb7388a04e7b4ba
rowantseng <wanju.ts@gmail.com>
2021-06-11 16:57:16 +0800

    update state hook to count using usestate hook

--- react-usestate-hook/src/components/App.jsx
+++ react-usestate-hook/src/components/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
--- react-usestate-hook/src/index.js
+++ react-usestate-hook/src/index.js
@@ -1,23 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-var count = 0;
-
-function increase() {
-  count++;
-  ReactDOM.render(
-    <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-    </div>,
-    document.getElementById("root")
-  );
-}
-
-ReactDOM.render(
-  <div className="container">
-    <h1>{count}</h1>
-    <button onClick={increase}>+</button>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
