4953f0bf2f60ca21e5ad61a88625e2d483b6f06a
rowantseng <wanju.ts@gmail.com>
2021-06-15 10:14:41 +0800

    complete react-changing-complex-state-practice

--- react-changing-complex-state-practice/src/components/App.jsx
+++ react-changing-complex-state-practice/src/components/App.jsx
@@ -4,8 +4,34 @@ function App() {
   const [contact, setContact] = useState({
     fName: "",
     lName: "",
-    email: ""
+    email: "",
   });
+
+  function handleChange(event) {
+    const {name, value} = event.target;
+
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email,
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value,
+        };
+      }
+    });
+  }
 
   return (
     <div className="container">
@@ -13,11 +39,11 @@ function App() {
         Hello {contact.fName} {contact.lName}
       </h1>
       <p>{contact.email}</p>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
-        <button>Submit</button>
+      <form onChange={handleChange}>
+        <input name="fName" placeholder="First Name" value={contact.fName} />
+        <input name="lName" placeholder="Last Name" value={contact.lName} />
+        <input name="email" placeholder="Email" value={contact.email} />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
