58767f83a4c1d1e7aeac4b75dff0d91986ace97f
rowantseng <wanju.ts@gmail.com>
2021-06-16 11:22:05 +0800

    complete keeper using prebuilt components

--- react-using-pre-built-components/src/components/CreateArea.jsx
+++ react-using-pre-built-components/src/components/CreateArea.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+
+  const [isClicked, setClick] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -26,23 +31,34 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function handleClick() {
+    setClick(true);
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+        {isClicked && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
         <textarea
           name="content"
           onChange={handleChange}
+          onClick={handleClick}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isClicked ? 3 : 1}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isClicked}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
